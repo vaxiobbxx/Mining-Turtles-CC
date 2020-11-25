e87ef7fa8d7c8ebee8169560fefa59a1ed9248f9
Peascalate <z.pelletier0@gmail.com>
2020-11-25 19:33:37 +0200

    switching over to VSC hollup

---
+++ startup.lua
@@ -0,0 +1,14 @@
+--[===[
+
+Mining Turtles CC
+file: startup.lua
+
+info:
+init file
+
+checks if async lib installed
+if not installed, installs
+if installed, loads using the async lib
+]===]--
+
+term.
